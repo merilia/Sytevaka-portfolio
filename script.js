6160d9d8c9926184e44017f20c3294a4fe8c43eb
Merili <meriliasu@gmail.com>
2015-02-04 12:27:47 +0200

    Initial
    Initial

---
+++ script.js
@@ -0,0 +1,12 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $('.slider-container').bxSlider({
+  mode: 'fade',
+  controls: true,
+  auto: true,
+  nextSelector: '#slider-next',
+  prevSelector: '#slider-prev',
+  nextText: '<img src="next.png" height="25" width="25"/>',
+  prevText: '<img src="previous.png" height="25" width="25"/>',
+});
+});
